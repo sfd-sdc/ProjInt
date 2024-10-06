572637fa2deb96cfb2aef3e71e3ff909ac7d761a
pedrosanto90 <pedro.bb.90@gmail.com>
2024-10-06 21:40:51 +0100

    add examplelayouts for the project

--- flaskr/app.py
+++ flaskr/app.py
@@ -1,16 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def home():
+def Home():
     data = {
-        'title': 'Projecto Inegrado',
-        'header': 'Home Page do Projecto Integrado',
-        'page': 'My Page'
+        'title' : 'Home',
+        'navTitle' : 'SDC Bank'
     }
     return render_template('index.html', data=data)
+
+@app.route('/user')
+def user():
+    # os dados a serem mostrados no frontend vao ser obtidos atraves de querys a base de dados
+    
+    user = {
+        'page': 'Dashboard',
+        'name' : 'Pedro Santo',
+        'email' : 'pedro@pedrosanto.pt',
+        'saldo' : '2000'
+    }
+    return render_template('user.html', data=user)
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000, host='0.0.0.0')
